0aae9a8c1c26cbc676f89ce1af66581b13ff19c5
Jay <108143448+swejenna@users.noreply.github.com>
2025-09-04 12:41:04 -0800

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { HomeHomeComponent } from './home-home/home-home.component';
+
+export const routes: Routes = [
+    { path: 'home', component: HomeHomeComponent }
+];
