e88fd0d4df533ba286d5f39748302063cdf23c4f
Jay <108143448+swejenna@users.noreply.github.com>
2025-09-10 11:27:29 -0800

    updated resume section to reflect a banker

--- src/app/home-home/home-home.component.ts
+++ src/app/home-home/home-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-home',
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './home-home.component.css'
 })
 export class HomeHomeComponent {
+  bankerName: string = "JennaTheBanker";
+  bankerCity: string = "Raleigh";
+  bankerState: string = "NC";
+  bankerPhone: string = "(123) 456-7890";
+  bankerEmail: string = "jennathebanker@me.com";
 
+  summary: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+  school: string = "North Carolina State University";
+  schoolDegree: string = "Bachelor of Science, Accounting";
+  schoolDates: string = "10/2024 - Present";
+  employerName: string = "J.P. Morgan Chase & Co";
+  jobTitle: string = "Banker/Teller"
+  employmentDates: string = "06/2024 - 08/2024";
+
+
+  constructor() { }
+  ngOnInit(): void { }
 }
